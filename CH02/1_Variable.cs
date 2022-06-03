b9b11a9fc65eb8ea0846226161c2e7fc85d3bcd5
Your Name <you@example.com>
2022-06-03 11:36:34 +0900

    2022/06/03수업예제

---
+++ CH02/1_Variable.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ *날짜 : 2022/06/02
+ *이름 : 김상우
+ *내용 : C# 변수실습하기 교재 p79
+ *
+ * 변수(variable)
+ * - 데이터를 처리하기 위해 생성되는 메모리 공간
+ * - 여러번 데이터를 변경할 수 있음
+ * 
+ * 상수(Constant)
+ * - 데이터를 변결할수 없음
+ */
+namespace CH02
+{
+    internal class _1_Variable
+    {
+        static void Main1(string[] args)
+        {
+            /////////////////////////////
+            // 변수
+            /////////////////////////////
+            int num1;//선언
+            num1 = 1;//대입
+
+            int num2 = 2; //선언과 초기화
+            int num3 = num1 + num2; //연산
+
+            Console.WriteLine("num3 : " + num3); // 결과확인
+
+
+            /////////////////////////////
+            // 상수
+            /////////////////////////////
+            const double PI = 3.141592;
+            const int NUM = 10;
+            //num = 20; <- 상수는 값 변경X
+            //num = 30;
+            Console.WriteLine("NUM : " + NUM);
+            Console.WriteLine("PI : " + PI);
+        }
+    }
+}
---
+++ CH02/4_InputTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ *날짜 : 2022/06/03
+ *이름 : 김상우
+ *내용 : C# 입력 실습하기 교재 p104
+ */
+namespace CH02
+{
+    internal class _4_InputTest
+    {
+        static void Main4(string[] args)
+        {
+            //이름입력
+            Console.Write("이름:");
+            string name = Console.ReadLine();
+
+            //나이입력
+            Console.Write("나이:");
+            string age = Console.ReadLine();
+
+            //주소입력
+            Console.Write("주소:");
+            string addr = Console.ReadLine();
+
+
+            Console.WriteLine("==================");
+            Console.WriteLine("이름 : "+name);
+            Console.WriteLine("나이 : "+age);
+            Console.WriteLine("주소 : "+addr);
+
+
+        }
+    }
+}
