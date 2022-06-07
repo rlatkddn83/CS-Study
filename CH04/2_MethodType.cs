68407af6930de83108ee91d4f6d206eb82d22519
Your Name <you@example.com>
2022-06-07 16:12:45 +0900

    2022/06/07수업예제

---
+++ CH04/2_MethodType.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 날짜 :  2022/06/07
+ * 이름 : 김상우
+ * 내용 : 메서드 타입 실습하기 교재 p265
+ */
+namespace CH04
+{
+    internal class _2_MethodType
+    {
+        static void Main2(string[] args)
+        {
+            //메서드 호출
+            double y1 = Type1(0.1);// 인자값 0.1 대입
+            double y2 = Type1(1.2);
+
+            Console.WriteLine("y1 : " + y1);
+            Console.WriteLine("y2 : " + y2);
+
+            Type2(true); // 반환값이 없으므로 대입연산자 필요없음
+            Type2(false);
+
+            string result = Type3();
+            Console.WriteLine("Type3 result : " + result);
+
+            Type4(); 
+        }
+
+        // type1 : 매개변수 o, 리턴값 o
+        public static double Type1(double x)
+        {
+            double y = x + 3.14;
+            return y;
+        }
+
+        // type2 : 매개변수 o, 리턴값 x
+        public static void Type2(bool status)
+        {
+            if (status)
+            {
+                Console.WriteLine("참 입니다.");
+            }
+            else
+            {
+                Console.WriteLine("거짓 입니다.");
+            }
+        }
+
+        // type3 : 매개변수 x, 리턴값 o
+        public static string Type3()
+        {
+            int n1 = 1, n2 = 2;
+
+            if(n1 > n2)
+                return "n1은 n2보다 크다.";
+            else
+                return "n1은 n2보다 작다.";
+        }
+
+        // type4 : 매개변수 x, 리턴값 x
+        public static void Type4()
+        {
+            double result = Type1(1.121);
+            Console.WriteLine("Type4 result : " + result);
+        }
+    }
+}
---
+++ CH04/4_MethodStack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 날짜 :  2022/06/07
+ * 이름 : 김상우
+ * 내용 : 메서드 출력전용 매개변수
+ */
+namespace CH04
+{
+    internal class _4_MethodStack
+    {
+        static void Main4(string[] args)
+        {
+            int result = 0;
+            int n1 = 1;
+            int n2 = 10;
+
+            result = Sum(n1, n2);
+
+            Console.WriteLine("result : "+result);
+        }
+
+        public static int Sum(int start, int end)
+        {
+            int total = 0;
+            for (int k = start; k <= end; k++)
+            {
+                total += k;
+            }
+
+            return total;
+        }
+    }
+}
