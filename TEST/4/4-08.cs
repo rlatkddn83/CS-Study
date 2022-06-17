cc544f799988709c278ae73857aa434097848cc9
Your Name <you@example.com>
2022-06-17 12:10:57 +0900

    교재예제

---
+++ TEST/4/4-08.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEST._4
+{
+    class Apple
+    {
+        public void Show()
+        {
+            Console.WriteLine("사과입니다.");
+        }
+    }
+    class Banana
+    {
+        public void Show()
+        {
+            Console.WriteLine("바나나입니다.");
+        }
+    }
+    class Grape
+    {
+        public void Show()
+        {
+            Console.WriteLine("포도입니다.");
+        }
+    }
+
+    internal class _4_08
+    {
+        static void Main(string[] args)
+        {
+            Apple apple = new Apple();
+            Banana banana = new Banana();
+            Grape grape = new Grape();
+        
+            Show(apple);
+            Show(banana);
+            Show(grape);
+        }
+        
+        public static void Show(object fruit)
+        {
+            if (fruit is Apple)
+            {
+                Apple apple = fruit as Apple;
+                apple.Show();
+            }
+            else if (fruit is Banana)
+            {
+                Banana banana = (Banana)fruit;
+                banana.Show();
+            }
+            else if (fruit is Grape)
+            {
+                Grape grape = fruit as Grape;
+                grape.Show();
+            }
+        }
+    }
+}
---
+++ TEST/4/4-10.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEST._4
+{
+    class Human
+    {
+        private string name;
+
+        public Human(string name)
+        {
+            this.name = name;
+        }
+
+        public virtual void Show()
+        {
+            Console.WriteLine(name + "입니다.");
+        }
+    }
+
+    class Worker : Human
+    {
+        public Worker(string name) : base(name)
+        { 
+        }
+
+        public override void Show()
+        {
+            base.Show();
+            Console.WriteLine("작업을 합니다.");
+        }
+    }
+    class Developer : Human
+    {
+        public Developer(string name) : base(name)
+        { 
+        }
+
+        public override void Show()
+        {
+            base.Show();
+            Console.WriteLine("개발을 합니다.");
+        }
+    }
+    class Professor : Human
+    {
+        public Professor(string name) : base(name)
+        { 
+        }
+
+        public override void Show()
+        {
+            base.Show();
+            Console.WriteLine("연구을 합니다.");
+        }
+    }
+
+    internal class _4_10
+    {
+        static void Main10(string[] args)
+        {
+            Worker worker = new Worker("김유신");
+            Developer dev = new Developer("김춘추");
+            Professor prof = new Professor("장보고");
+
+            Human[] people = {worker, dev, prof};
+
+            foreach(Human human in people)
+            {
+                human.Show();
+                Console.WriteLine();
+            }
+        }
+    }
+}
