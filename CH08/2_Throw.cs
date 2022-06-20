7ae5ca0ad30e6bd46d86830224fbde503a89dc23
Your Name <you@example.com>
2022-06-20 11:49:00 +0900

    2022/06/20수업예제

---
+++ CH08/2_Throw.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 날짜 : 2022/06/20
+ * 이름 : 김상우
+ * 내용 : 예외처리 Throw 실습하기 교재 p467
+ */
+namespace CH08
+{
+    class Box
+    {
+        private int width;
+        private int height;
+
+        public int Width { 
+            get { return width; } 
+            set 
+            {
+                if (value > 0)
+                {
+                    width = value; 
+                }
+                else
+                {
+                    // 가로길이가 0보다 작을 경우 예외를 강제 발생
+                    throw new Exception("너비는 0보다 커야합니다.");
+                }
+            } 
+        }
+        public int Height { 
+            get { return height; } 
+            set 
+            {
+                if (value > 0)
+                {
+                    height = value;
+                }
+                else
+                {
+                    // 세로길이가 0보다 작을 경우 예외를 강제 발생
+                    throw new Exception("높이는 0보다 커야합니다.");
+                }
+            } 
+        }
+
+        public Box(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public void Area()
+        {
+            Console.WriteLine("Box 넓이 : " + width * height);
+        }
+
+    }
+
+    internal class _2_Throw
+    {
+        static void Main2(string[] args)
+        {
+            try
+            {
+                Box box1 = new Box(10, 10);
+                box1.Area();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+    }
+}
---
+++ CH08/3_FileIO.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 날짜 : 2022/06/20
+ * 이름 : 김상우
+ * 내용 : 파일 입출력 실습하기
+ */
+namespace CH08
+{
+
+    internal class _3_FileIO
+    {
+        static void Main(string[] args)
+        {
+            /////////////////////////
+            //파일입력(Read)
+            /////////////////////////
+            string path1 = "C:\\Users\\1302\\Desktop\\sample1.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+            try
+            {
+                // 스트림 생성
+                fs = new FileStream(path1, FileMode.Open, FileAccess.Read);
+
+                // 텍스트 하일 전용 보조 스트림 연결
+                sr = new StreamReader(fs);
+
+                // 파일 읽기
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                // 파일 닫기(자원 해제)
+                sr.Close();
+                fs.Close();
+            }
+            /////////////////////////
+            //파일출력(Write)
+            /////////////////////////
+            string path2 = "C:\\Users\\1302\\Desktop\\sample2.txt";
+            FileStream outfs = null;
+            StreamWriter sw = null;
+
+            try
+            {
+                // 스트림 생성
+                outfs = new FileStream(path2, FileMode.OpenOrCreate, FileAccess.Write);
+                sw = new StreamWriter(outfs);
+
+                // 파일 쓰기
+                sw.WriteLine("Welcome World!");
+                sw.WriteLine("Welcome Korea!");
+                sw.WriteLine("Welcome Busan!");
+
+                for (int i = 2; i < 10; i++)
+                {
+                    for (int j = 1; j < 10; j++)
+                    {
+                        sw.WriteLine("{0} * {1} = {2}", i, j, i * j);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                // 파일 닫기
+                sw.Close();
+                outfs.Close();
+            }
+            /////////////////////////
+            //파일 읽기/쓰기
+            /////////////////////////
+            string f1 = "C:\\Users\\1302\\Desktop\\sample1.txt";
+            string f3 = "C:\\Users\\1302\\Desktop\\sample3.txt";
+
+            try
+            {
+                // using 선언으로 스트림 자원 해제 자동 실행
+                using var reader = new StreamReader(new FileStream(f1, FileMode.Open));
+                using var writer = new StreamWriter(new FileStream(f3, FileMode.Create));
+
+                string txt;
+
+                while ((txt = reader.ReadLine()) != null)
+                {
+                    writer.WriteLine(txt);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("프로그램 종료...");
+        }
+    }
+}
