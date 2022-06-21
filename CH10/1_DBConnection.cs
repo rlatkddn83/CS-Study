5aaaf248809d95e8188d29680f1db09bc6500da1
Your Name <you@example.com>
2022-06-21 12:14:59 +0900

    2022/06/21수업예제

---
+++ CH10/1_DBConnection.cs
@@ -0,0 +1,52 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 날짜 : 2022/06/21
+ * 이름 : 김상우
+ * 내용 : 데이터베이스 프로그래밍 실습
+ * 
+ * MySQL 패키지 설치
+ *  도구 - NuGet 패키지 관리자 - 솔루션용 NuGet 패키지 관리... - mysql 검색 - MySQL 설치
+ */
+namespace CH10
+{
+    internal class _1_DBConnection
+    {
+        static void Main1(string[] args)
+        {
+            // 데이터베이스 접속 정보
+            string server = "127.0.0.1";
+            string port = "3306";
+            string username = "tester";
+            string password = "1q2w3e";
+            string database = "testdb";
+
+            // 데이터베이스 커넥션 생성
+            string strconn = $"server={server};port={port};username={username};password={password};database={database}";
+            MySqlConnection conn = new MySqlConnection(strconn);
+
+            try
+            {
+                // 데이터베이스 접속
+                conn.Open();
+                Console.WriteLine("접속 성공...");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("접속 실패 : " + e.Message);
+            }
+            finally
+            {
+                conn.Close();
+                Console.WriteLine("접속 종료...");
+
+            }
+
+        }
+    }
+}
---
+++ CH10/2_Insert.cs
@@ -0,0 +1,58 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 날짜 : 2022/06/21
+ * 이름 : 김상우
+ * 내용 : 데이터베이스 프로그래밍 실습
+ * 
+ * MySQL 패키지 설치
+ *  도구 - NuGet 패키지 관리자 - 솔루션용 NuGet 패키지 관리... - mysql 검색 - MySQL 설치
+ */
+namespace CH10
+{
+    internal class _2_Insert
+    {
+        static void Main2(string[] args)
+        {
+            // 데이터베이스 접속 정보
+            string server = "127.0.0.1";
+            string port = "3306";
+            string username = "tester";
+            string password = "1q2w3e";
+            string database = "testdb";
+
+            // 데이터베이스 커넥션 생성
+            string strconn = $"server={server};port={port};username={username};password={password};database={database}";
+            MySqlConnection conn = new MySqlConnection(strconn);
+
+            try
+            {
+                // 데이터베이스 접속
+                conn.Open();
+                Console.WriteLine("접속 성공...");
+
+                // SQL 실행
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "INSERT INTO `user1` VALUES ('c101','김유신','010-1234-1010',27)";
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("접속 실패 : " + e.Message);
+            }
+            finally
+            {
+                conn.Close();
+                Console.WriteLine("접속 종료...");
+
+            }
+
+
+        }
+    }
+}
