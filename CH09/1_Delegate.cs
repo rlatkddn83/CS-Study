6c91856da4b222e281a008fe96ce3aaaa97af3e4
Your Name <you@example.com>
2022-06-21 12:14:45 +0900

    2022/06/21수업예제

---
+++ CH09/1_Delegate.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 날짜 : 2022/06/21
+ * 이름 : 김상우
+ * 내용 : 대리자(Delegate) 실습하기 교재 p493
+ * 
+ * 대리자(Delegate)
+ *  - 대리자는 메서드에 대한 참조를 나타내는 형식으로 메서드를 대신 호출 해주는 역할
+ *  - 대리자를 익명 메서드로 많이 활용한다.
+ */
+namespace CH09
+{
+    // 대리자 선언
+    public delegate int MyDelegate(int a, int b);
+    public delegate int SumDelegate(int[] arr);
+
+    internal class _1_Delegate
+    {
+        static void Main1(string[] args)
+        {
+            // 대리자 초기화(메서드 참조)
+            MyDelegate my1 = new MyDelegate(Plus);
+            MyDelegate my2 = new MyDelegate(Minus);
+
+            // 대리자 실행
+            int r1 = my1(1, 2);
+            int r2 = my2(2, 3);
+
+            Console.WriteLine("r1 : " + r1);
+            Console.WriteLine("r1 : " + r1);
+
+            // 대리자 활용(메서드의 매개변수로 대리자 선언)
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            int rs1 = Sum(arr,OddSum); // 행위를 전달하기 위해 대리자 사용
+            int rs2 = Sum(arr,EvenSum);
+
+            // 대리자를 익명 메서드로 활용
+            var md = delegate (int x, int y)
+            {
+                return x * y;
+            };
+
+            int result1 = md(2, 3);
+            int result2 = md(3, 4);
+
+            Console.WriteLine("result1 : " + result1);
+            Console.WriteLine("result2 : " + result2);
+
+            // 익명 메서드를 매개변수로 갖는 컬랙션 메서드
+            List<int> dataset = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            dataset.ForEach(delegate (int n)
+            {
+                Console.Write(n + " ");
+            });
+
+            foreach(int n in dataset)
+            {
+                Console.Write(n + " ");
+            }
+
+        }
+
+        public static int Plus(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int OddSum(int[] arr)
+        {
+            int sum = 0;
+            foreach(int n in arr)
+            {
+                if (n % 2 == 1)
+                {
+                    sum += n;
+                }
+            }
+            return sum;
+        }
+
+        public static int EvenSum(int[] arr)
+        {
+            int sum = 0;
+            foreach(int n in arr)
+            {
+                if (n % 2 == 0)
+                {
+                    sum += n;
+                }
+            }
+            return sum;
+        }
+
+        public static int Sum(int[] arr, SumDelegate sumDelegate)
+        {
+            return sumDelegate(arr);
+        }
+    }
+}
---
+++ CH09/3_Linq.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 날짜 : 2022/06/21
+ * 이름 : 김상우
+ * 내용 : Linq 실습하기 교재 p525
+ * 
+ * Linq
+ *  - Linq(Language Intergrated Query)는 컬렉션 형태의 데이터를 가공처리 하기 위한 C# 질의언어
+ *  - Linq를 사용해서 DB와 함께 데이터를 쉽게 관리
+ */
+namespace CH09
+{
+    class Member
+    {
+        private string uid;
+        private string name;
+        private int age;
+
+        public Member(string uid, string name, int age)
+        {
+            this.Uid = uid;
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Uid { get => uid; set => uid = value; }
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+    }
+
+    internal class _3_Linq
+    {
+        static void Main3(string[] args)
+        {
+            // 컬랙션 생성
+            List<int> dataset = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            // 짝수 데이터 리스트 생성
+            List<int> result1 = new List<int>();
+            List<int> result2 = new List<int>();
+
+            //방법1, for문 이용
+            foreach (int n in dataset)
+            {
+                if (n % 2 == 0)
+                {
+                    result1.Add(n);
+                }
+            }
+
+            //방법2, 람다식 이용
+            dataset.ForEach((n) => {
+                if (n%2 == 0)
+                {
+                    result2.Add(n);
+                }
+            });
+
+            //방법3. Linq 이용
+            var result3 = from n in dataset where n % 2 == 0 select n;
+
+            Console.WriteLine("result1 : " + String.Join(", ", result1));
+            Console.WriteLine("result2 : " + String.Join(", ", result2));
+            Console.WriteLine("result3 : " + String.Join(", ", result3));
+
+            // 데이터가 5이상 큰 순서대로 출력
+            var result4 = from n in dataset 
+                          where n >= 5 
+                          orderby n descending 
+                          select n ;
+            Console.WriteLine("result4 : " + String.Join(", ", result4));
+
+            // 객체 리스트 Linq 이용
+            List<Member> members = new List<Member>();
+            members.Add(new Member("a101", "김유신", 25));
+            members.Add(new Member("a102", "김춘추", 23));
+            members.Add(new Member("a103", "장보고", 35));
+            members.Add(new Member("a104", "강감찬", 45));
+            members.Add(new Member("a105", "이순신", 55));
+
+            var result5 = from member in members
+                          where member.Age >= 30
+                          orderby member.Name ascending
+                          select member;
+
+            foreach ( Member n in result5)
+            {
+                Console.WriteLine("{0}, {1}, {2}",n.Uid, n.Name, n.Age);
+            }
+
+        }
+    }
+}
